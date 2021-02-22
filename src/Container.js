f86e3a621f4a55fc5d16cd140534fd1e0eb6bcec
KireevAlex <kireeva.a91@gmail.com>
2021-02-22 18:03:08 +0200

    HW_1

---
+++ src/Container.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+
+
+export default class Сontainer extends Component{
+    render () {
+        return (
+            <div>did my homework late. Sorry)</div>
+        );
+    };
+};
